9cb5b4adb325ec1177cf66628bcd0ca7e79be047
wphu <wanpengh@gmail.com>
2017-07-20 16:44:19 +0800

    update some python scripts

--- tools/pyPlot_examples/draw1d_velocity.py
+++ tools/pyPlot_examples/draw1d_velocity.py
@@ -8,27 +8,27 @@ import matplotlib
 matplotlib.use('Agg')
 
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 from matplotlib.ticker import MaxNLocator
 from matplotlib import cm
-
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 
-import numpy as np
-from numpy import arange, sin, pi
-
-import ConfigParser
-
-import h5py as h5
-import numpy as np
-import matplotlib.pyplot as plt
-import math
 from matplotlib.ticker import ScalarFormatter
 yformatter = ScalarFormatter()
 yformatter.set_powerlimits((-3,3))
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.ticker as ticker
+
+
+from numpy import arange, sin, pi
+import numpy as np
+
+import h5py as h5
+import math
+
 
 
 font={	'family' : 'sans-serif',
@@ -36,10 +36,16 @@ font={	'family' : 'sans-serif',
 	'size' : 8,
 	}
 
-mpl.rcParams['text.usetex'] = True
-mpl.rcParams['text.latex.unicode'] = True
+#mpl.rcParams['text.usetex'] = True
+#mpl.rcParams['text.latex.unicode'] = True
 mpl.rcParams['font.family'] = 'sans-serif'
 #mpl.rcParams['mathtext.default'] = 'regular'
+#mpl.rcParams['mathtext.default'] = 'it'
+mpl.rcParams['mathtext.fontset'] = 'stix'
+#mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.it'] = 'serif'
+#mpl.rcParams['pdf.fonttype'] = 3
+
 
 mpl.rcParams['font.size'] = 16
 mpl.rcParams['axes.linewidth'] = 2.0
@@ -54,8 +60,14 @@ mpl.rcParams['lines.linewidth'] = 2.0
 mpl.rcParams['grid.linestyle'] = ":"
 mpl.rcParams['grid.color'] = "black"
 
-def get_axis_limits(ax, x_scale=0, y_scale=1.02):
-    return ax.get_xlim()[1]*x_scale, ax.get_ylim()[1]*y_scale
+
+
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+
+def get_axis_limits(ax, x_scale=0, y_scale=1.12):
+    #print ax.get_xlim(), ax.get_ylim()
+    return ax.get_xlim()[1]*x_scale, ax.get_ylim()[1] + ( ax.get_ylim()[1] - ax.get_ylim()[0] ) * (y_scale - 1.0)
 
 
 
@@ -66,7 +78,10 @@ def get_axis_limits(ax, x_scale=0, y_scale=1.02):
 ##inite the fig of matplotlib
 fig=plt.figure(figsize=(10,8))
 fig.subplots_adjust(top=0.9,bottom=0.1,wspace=0.5,hspace=0.4)
+
 t = 19
+
+label_fontsize = 20
 
 
 ##read data from file
@@ -82,43 +97,92 @@ print dims
 nx = dims[3]
 
 
-dx = 1.0e-2  # unit (mm)
+dx = 0.5e-2  # unit (mm)
 x = np.linspace(0, nx * dx, nx)
 
 xmin = x.min()
 xmax = x.max()
 
 # ion sound speed
-Va1 = math.sqrt( 25.0 * 1.602e-19 / (2.0 * 1.67262158e-27) ) / 1.0e6
-Va2 = math.sqrt( 60.0 * 1.602e-19 / (2.0 * 1.67262158e-27) ) / 1.0e6
+Va1 = math.sqrt( 111.3 * 1.602e-19 / (2.0 * 1.67262158e-27) ) 
+Va2 = math.sqrt( 120.0 * 1.602e-19 / (2.0 * 1.67262158e-27) )
 
 ##============ Parallel Velocity ======================================================
 sp_temp1=fig.add_subplot(2,1,1)
-'''
-val = f["/Fields/Vparallel_global_e_avg"]
-val = val[...]
-val_1d = np.transpose(val[t, 0, 0, :])
-val_1d = val_1d / 1.0e6
-cf_temp1=sp_temp1.plot(x, val_1d, label = "Electron")
-'''
+sp_temp1.yaxis.set_major_formatter(yformatter)
 
 val = f["/Fields/Vparallel_global_D1_avg"]
 val = val[...]
 val_1d = np.transpose(val[t, 0, 0, :])
-val_1d = val_1d / 1.0e6
-cf_temp1=sp_temp1.plot(x, val_1d, label = "D+1")
+val1_1d = val_1d
+cf_temp1=sp_temp1.plot(x, val1_1d, label = r"$D^+\ ion$")
+
+val = f["/Fields/Vparallel_global_e_avg"]
+val = val[...]
+val_1d = np.transpose(val[t, 0, 0, :])
+val0_1d = val_1d
+cf_temp1=sp_temp1.plot(x, val0_1d, label = r"$Electron$")
+
+
+
+x0_ax_inset0 = 0.0
+y0_ax_inset0 = val0_1d.max() * 0.1
+width0_ax_inset0 = 8.0
+height0_ax_inset0 = val0_1d.max() * 0.8
+ax_inset0 = inset_axes(sp_temp1, width = "60%", height = "80%" , bbox_to_anchor = (x0_ax_inset0, y0_ax_inset0, width0_ax_inset0, height0_ax_inset0), bbox_transform = sp_temp1.transData)
+
+ax_inset0.yaxis.set_major_formatter(yformatter)
+ax_inset0.plot(x, val1_1d)
+ax_inset0.plot(x, val0_1d)
+ax_inset0.set_xlim(0.0, 0.2)
+#ax_inset0.set_ylim(0.0, 0.3)
+
 
 # Plot ion sound speed
-sp_temp1.axhline(y = Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
-sp_temp1.axhline(y = -Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
-sp_temp1.axhline(y = Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
-sp_temp1.axhline(y = -Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = -Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = -Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
 
 sp_temp1.legend()
 sp_temp1.set_xlim((xmin, xmax))
 #sp_temp1.set_yticks(np.arange(0,y.max(),100))
-sp_temp1.set_xlabel('x(mm)')
-sp_temp1.set_ylabel('Vp (1.0e6 m/s)')
+sp_temp1.set_xlabel(r"$x\ (mm)$", fontsize = label_fontsize)
+sp_temp1.set_ylabel(r"$V\ (m/s)$", fontsize = label_fontsize)
+
+sp_temp1.annotate('(a)', xy=get_axis_limits(sp_temp1), annotation_clip=False)
+
+##============ Ion Parallel Velocity ======================================================
+sp_temp1=fig.add_subplot(2,1,2)
+sp_temp1.yaxis.set_major_formatter(yformatter)
+
+
+val = f["/Fields/Vparallel_global_D1_avg"]
+val = val[...]
+val_1d = np.transpose(val[t, 0, 0, :])
+val1_1d = val_1d
+cf_temp1=sp_temp1.plot(x, val1_1d, label = "D+1")
+
+
+
+# Plot ion sound speed
+#sp_temp1.axhline(y = Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
+sp_temp1.axhline(y = -Va1, xmin = 0.0, xmax = 1.0, c="red",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
+#sp_temp1.axhline(y = -Va2, xmin = 0.0, xmax = 1.0, c="blue",zorder=0, clip_on=False)
+
+#sp_temp1.legend()
+xmin = 0.0
+xmax = 0.25
+ymin = val1_1d.min() * 1.1
+ymax = 0.0
+sp_temp1.set_xlim((xmin, xmax))
+sp_temp1.set_ylim((ymin, ymax))
+#sp_temp1.set_yticks(np.arange(0,y.max(),100))
+sp_temp1.set_xlabel(r"$x\ (mm)$", fontsize = label_fontsize)
+sp_temp1.set_ylabel(r"$V_{D^+}\ (m/s)$", fontsize = label_fontsize)
+
+sp_temp1.annotate('(b)', xy=get_axis_limits(sp_temp1), annotation_clip=False)
 
 
 '''
@@ -145,7 +209,7 @@ sp_temp1.set_ylabel('Vx (1.0e6 m/s)')
 '''
 
 
-fig.savefig("velocity.png", dpi = 300)
+fig.savefig("velocity.pdf", dpi = 300)
 ##fig.show()       #when the program finishes,the figure disappears
 #plt.axis('equal')
 plt.show()         #The command is OK
